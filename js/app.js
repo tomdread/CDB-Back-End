aa2ceb68806749397cc50f4c09b4d7e81a5912e8
Thomas Rooney <tom@dreadserver.com>
2018-07-31 13:58:07 +0100

    added favicon

--- js/app.js
+++ js/app.js
@@ -3,29 +3,29 @@
   'use strict';
 
   var ENTER_KEY = 13;
-  var newTodoDom = document.getElementById('new-todo');
+  var newJobDom = document.getElementById('new-job');
   var syncDom = document.getElementById('sync-wrapper');
 
   // EDITING STARTS HERE (you dont need to edit anything above this line)
 
-  var db = new PouchDB('todos');
-  var remoteCouch = 'http://192.168.43.95:5984/todos';
+  var db = new PouchDB('jobs');
+  var remoteCouch = 'http://192.168.43.95:5984/jobs';
   
   db.info(function(err, info) {
     db.changes({
       since: info.update_seq,
       live: true
-    }).on('change', showTodos);
+    }).on('change', showJobs);
   });
 
-  // We have to create a new todo document and enter it in the database
-  function addTodo(text) {
-    var todo = {
+  // We have to create a new job document and enter it in the database
+  function addJob(text) {
+    var job = {
       _id: new Date().toISOString(),
       title: text,
       completed: false
     };
-    db.put(todo).then(function (result) {
+    db.put(job).then(function (result) {
       console.log("everything is A-OK");
       console.log(result);
     }).catch(function (err) {
@@ -34,35 +34,35 @@
     });
   }
 
-  // Show the current list of todos by reading them from the database
-  function showTodos() {
+  // Show the current list of jobs by reading them from the database
+  function showJobs() {
     db.allDocs({include_docs: true, descending: true}).then(function(doc) {
-      redrawTodosUI(doc.rows);
+      redrawJobsUI(doc.rows);
     }).catch(function (err) {
       console.log(err);
     });
   }
 
-  function checkboxChanged(todo, event) {
-    todo.completed = event.target.checked;
-    console.log(todo);
-    db.put(todo);
+  function checkboxChanged(job, event) {
+    job.completed = event.target.checked;
+    console.log(job);
+    db.put(job);
   }
 
-  // User pressed the delete button for a todo, delete it
-  function deleteButtonPressed(todo) {
-    db.remove(todo);
+  // User pressed the delete button for a job, delete it
+  function deleteButtonPressed(job) {
+    db.remove(job);
   }
 
-  // The input box when editing a todo has blurred, we should save
-  // the new title or delete the todo if the title is empty
-  function todoBlurred(todo, event) {
+  // The input box when editing a job has blurred, we should save
+  // the new title or delete the job if the title is empty
+  function jobBlurred(job, event) {
     var trimmedText = event.target.value.trim();
     if (!trimmedText) {
-      db.remove(todo);
+      db.remove(job);
     } else {
-      todo.title = trimmedText;
-      db.put(todo);
+      job.title = trimmedText;
+      db.put(job);
     }
   }
 
@@ -81,38 +81,38 @@
     syncDom.setAttribute('data-sync-state', 'error');
   }
 
-  // User has double clicked a todo, display an input so they can edit the title
-  function todoDblClicked(todo) {
-    var div = document.getElementById('li_' + todo._id);
-    var inputEditTodo = document.getElementById('input_' + todo._id);
+  // User has double clicked a job, display an input so they can edit the title
+  function jobDblClicked(job) {
+    var div = document.getElementById('li_' + job._id);
+    var inputEditJob = document.getElementById('input_' + job._id);
     div.className = 'editing';
-    inputEditTodo.focus();
+    inputEditJob.focus();
   }
 
   // If they press enter while editing an entry, blur it to trigger save
   // (or delete)
-  function todoKeyPressed(todo, event) {
+  function jobKeyPressed(job, event) {
     if (event.keyCode === ENTER_KEY) {
-      var inputEditTodo = document.getElementById('input_' + todo._id);
-      inputEditTodo.blur();
+      var inputEditJob = document.getElementById('input_' + job._id);
+      inputEditJob.blur();
     }
   }
 
-  // Given an object representing a todo, this will create a list item
+  // Given an object representing a job, this will create a list item
   // to display it.
-  function createTodoListItem(todo) {
+  function createJobListItem(job) {
     var checkbox = document.createElement('input');
     checkbox.className = 'toggle';
     checkbox.type = 'checkbox';
-    checkbox.addEventListener('change', checkboxChanged.bind(this, todo));
+    checkbox.addEventListener('change', checkboxChanged.bind(this, job));
 
     var label = document.createElement('label');
-    label.appendChild( document.createTextNode(todo.title));
-    label.addEventListener('dblclick', todoDblClicked.bind(this, todo));
+    label.appendChild( document.createTextNode(job.title));
+    label.addEventListener('dblclick', jobDblClicked.bind(this, job));
 
     var deleteLink = document.createElement('button');
     deleteLink.className = 'destroy';
-    deleteLink.addEventListener( 'click', deleteButtonPressed.bind(this, todo));
+    deleteLink.addEventListener( 'click', deleteButtonPressed.bind(this, job));
 
     var divDisplay = document.createElement('div');
     divDisplay.className = 'view';
@@ -120,19 +120,19 @@
     divDisplay.appendChild(label);
     divDisplay.appendChild(deleteLink);
 
-    var inputEditTodo = document.createElement('input');
-    inputEditTodo.id = 'input_' + todo._id;
-    inputEditTodo.className = 'edit';
-    inputEditTodo.value = todo.title;
-    inputEditTodo.addEventListener('keypress', todoKeyPressed.bind(this, todo));
-    inputEditTodo.addEventListener('blur', todoBlurred.bind(this, todo));
+    var inputEditJob = document.createElement('input');
+    inputEditJob.id = 'input_' + job._id;
+    inputEditJob.className = 'edit';
+    inputEditJob.value = job.title;
+    inputEditJob.addEventListener('keypress', jobKeyPressed.bind(this, job));
+    inputEditJob.addEventListener('blur', jobBlurred.bind(this, job));
 
     var li = document.createElement('li');
-    li.id = 'li_' + todo._id;
+    li.id = 'li_' + job._id;
     li.appendChild(divDisplay);
-    li.appendChild(inputEditTodo);
+    li.appendChild(inputEditJob);
 
-    if (todo.completed) {
+    if (job.completed) {
       li.className += 'complete';
       checkbox.checked = true;
     }
@@ -140,27 +140,27 @@
     return li;
   }
 
-  function redrawTodosUI(todos) {
-    var ul = document.getElementById('todo-list');
+  function redrawJobsUI(jobs) {
+    var ul = document.getElementById('job-list');
     ul.innerHTML = '';
-    todos.forEach(function(todo) {
-      ul.appendChild(createTodoListItem(todo.doc));
+    jobs.forEach(function(job) {
+      ul.appendChild(createJobListItem(job.doc));
     });
   }
 
-  function newTodoKeyPressHandler( event ) {
+  function newJobKeyPressHandler( event ) {
     if (event.keyCode === ENTER_KEY) {
-      addTodo(newTodoDom.value);
-      newTodoDom.value = '';
+      addJob(newJobDom.value);
+      newJobDom.value = '';
     }
   }
 
   function addEventListeners() {
-    newTodoDom.addEventListener('keypress', newTodoKeyPressHandler, false);
+    newJobDom.addEventListener('keypress', newJobKeyPressHandler, false);
   }
 
   addEventListeners();
-  showTodos();
+  showJobs();
 
   if (remoteCouch) {
     sync();
